38a844c9cbf2caf2a35d67ae319a18b827e3abc0
balvinder singh <balvindersi2@gmail.com>
2019-09-24 19:16:45 +0530

    Added how to use

--- app/src/main/java/tired/coder/mediumarticlereader/MainActivity.java
+++ app/src/main/java/tired/coder/mediumarticlereader/MainActivity.java
@@ -1,17 +1,29 @@
 package tired.coder.mediumarticlereader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  Intent intent = new Intent(this,ArticleActivity.class);
-        //startActivity(intent);
+        toolbar = findViewById(R.id.maintoolbar);
+        toolbar.setTitle("Medium Article Reader");
     }
 }
